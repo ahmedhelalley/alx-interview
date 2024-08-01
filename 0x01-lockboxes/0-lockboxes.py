50aa7f43e857d2756f3d9297e6a8df023c0986f0
ahmedhelalley <a.helalley@gmail.com>
2024-08-01 17:31:42 +0300

    Completed tasks

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Defines canUnlockAll function.."""
+
+
+def canUnlockAll(boxes):
+    """Return True if all boxes can be opened, else return False"""
+    unlocked_boxes = [0]
+    keys = []
+    keys.extend(boxes[0])
+    boxes_length = len(boxes)
+
+    while keys:
+        key = keys.pop()
+
+        if key not in unlocked_boxes and key < boxes_length:
+            keys.extend(boxes[key])
+            unlocked_boxes.append(key)
+
+    return len(unlocked_boxes) == boxes_length
